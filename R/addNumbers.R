455f8ca310364776df4af8e69ccaea8b59eb9428
cmatkhan <chase.mateusiak@gmail.com>
2021-03-10 10:44:16 -0600

    first commit

---
+++ R/addNumbers.R
@@ -0,0 +1,32 @@
+#' Add two numbers together
+#'
+#' @param x a number
+#' @param y another number
+#'
+#' @return the sum of \code{x} \code{y}
+#'
+#' @examples
+#'  add(1,1)
+#'  add(10,1)
+#'
+#' @note this example is taken from the roxygen documnetation here: https://cran.r-project.org/web/packages/roxygen2/vignettes/roxygen2.html
+#'
+#' @export
+add = function(x, y){
+  # don't use the 'ticked' comments in here!
+  x + y
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
